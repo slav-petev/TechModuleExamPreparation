0ccf145e7a44764a02495ce55f0e1365ba767ff7
slapet <slapet@CB-BG-SLAPET>
2018-02-13 18:23:31 +0200

    Solved first problem with time limit

--- Prepararation/Preparation.Pokemon/PokemonSolution.cs
+++ Prepararation/Preparation.Pokemon/PokemonSolution.cs
@@ -6,7 +6,28 @@ namespace Preparation.Pokemon
     {
         public static void Main()
         {
-            
+            uint pokePower = uint.Parse(Console.ReadLine());
+            uint pokeTargetDistance = uint.Parse(Console.ReadLine());
+            byte exhaustionFactor = byte.Parse(Console.ReadLine());
+
+            uint targetsPoked = 0;
+            uint changingPokePower = pokePower;
+            uint halfPokePower = pokePower / 2;
+            while (changingPokePower >= pokeTargetDistance)
+            {
+                if (changingPokePower == halfPokePower && exhaustionFactor != 0)
+                {
+                    changingPokePower /= exhaustionFactor;
+                }
+                else
+                {
+                    changingPokePower -= pokeTargetDistance;
+                    targetsPoked++;
+                }  
+            }
+
+            Console.WriteLine(changingPokePower);
+            Console.WriteLine(targetsPoked);
         }
     }
 }
